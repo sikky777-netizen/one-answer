3cfa26415e66345c71c8c4832644485b002e7da0
sikky777-netizen <sikky777@gmail.com>
2026-09-17 10:39:08 +0900

    Bump PWA cache version for AI mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="one-answer-v2";
+const CACHE="one-answer-v3-ai";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
