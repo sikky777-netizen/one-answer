e6d37e5065be82de7a6f74e87646df7cebd1a365
sikky777-netizen <sikky777@gmail.com>
2026-09-18 15:07:53 +0900

    Replace repeated book cover label with 마음의 힌트

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="one-answer-v13-stable";
+const CACHE="one-answer-v14-stable";
 const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./install-icon-v13-192.png","./install-icon-v13-512.png","./dubu-avatar.jpg","./og-image.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
