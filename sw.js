4247d620ebe861ceb3f86c1782202fc19fcf8c53
sikky777-netizen <sikky777@gmail.com>
2026-09-18 11:56:53 +0900

    Fit mobile app to one screen and replace question mark logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="one-answer-v7-stable";
+const CACHE="one-answer-v8-stable";
 const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./dubu-avatar.jpg","./og-image.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
