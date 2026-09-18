8b14f419a60e2ed61b1515fdd8a7891b945e930b
sikky777-netizen <sikky777@gmail.com>
2026-09-18 13:24:32 +0900

    Fill install icon with large Korean title and outer gold border

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="one-answer-v11-stable";
-const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./install-icon-v11-192.png","./install-icon-v11-512.png","./dubu-avatar.jpg","./og-image.jpg"];
+const CACHE="one-answer-v12-stable";
+const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./install-icon-v12-192.png","./install-icon-v12-512.png","./dubu-avatar.jpg","./og-image.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
