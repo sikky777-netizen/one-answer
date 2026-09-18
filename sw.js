3616f190018cf7ee8aca2db459d2f230d9192c8d
sikky777-netizen <sikky777@gmail.com>
2026-09-18 21:07:45 +0900

    Replace taxi advertisement with click-to-call TV installation banner

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="one-answer-v23-stable";
-const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./install-icon-v13-192.png","./install-icon-v13-512.png","./dubu-avatar.jpg","./og-image.jpg","./taxi-soft-v3.webp"];
+const CACHE="one-answer-v24-tv-install";
+const ASSETS=["./","./index.html","./share.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./install-icon-v13-192.png","./install-icon-v13-512.png","./dubu-avatar.jpg","./og-image.jpg","./tv-install-320x80.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
